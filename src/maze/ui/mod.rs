11413b4faa065a831a8fc0dc2cc6126fe18e5053
Filip Barabas <filip.barabas@student.tuke.sk>
2024-12-01 22:34:05 +0100

    Separate ui container builder

--- src/maze/ui/mod.rs
+++ src/maze/ui/mod.rs
@@ -1,12 +1,11 @@
+mod builders;
 mod buttons;
 mod sliders;
 
-use super::{
-    states::MazeState,
-    window::{UI_WINDOW_HEIGHT, UI_WINDOW_WIDTH},
-};
+use super::states::MazeState;
 
 use bevy::prelude::*;
+use builders::spawn_ui_container;
 use buttons::{
     builders::spawn_action_button, interactions::button_state_system, ActionButtonsPlugin,
 };
@@ -25,20 +24,7 @@ pub struct LoadMazeButton;
 pub struct SaveMazeButton;
 
 pub fn generate_ui(mut commands: Commands) {
-    commands
-        .spawn(Node {
-            position_type: PositionType::Absolute,
-            display: Display::Flex,
-            flex_direction: FlexDirection::Column,
-            row_gap: Val::Px(10.0),
-            align_self: AlignSelf::Center,
-            align_items: AlignItems::Center,
-            justify_content: JustifyContent::Center,
-            width: Val::Px(UI_WINDOW_WIDTH), //
-            height: Val::Px(UI_WINDOW_HEIGHT),
-            right: Val::Px(0.0),
-            ..default()
-        })
+    spawn_ui_container(&mut commands)
         .with_children(|builder| {
             spawn_action_button(builder, GenerateMazeButton, "Generate maze");
         })
